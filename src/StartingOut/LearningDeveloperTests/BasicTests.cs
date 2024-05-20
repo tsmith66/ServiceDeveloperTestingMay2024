86d9bf1d9536c8a7c83c40862ecc27cdbbac127e
Tim Smith <tsmith661@sbcglobal.net>
2024-05-20 11:50:58 -0400

    Morning Day 1

---
+++ src/StartingOut/LearningDeveloperTests/BasicTests.cs
@@ -0,0 +1,31 @@
+﻿namespace LearningDeveloperTests;
+public class BasicTests
+{
+    [Fact]
+
+    public void CanAddTwoNumbers()
+
+    {
+        //GIVEN (Arrange) - this should recreate the world from scratch
+        int a = 10, b = 20;
+        int c;
+
+        //The important part - what we are actually testing
+        //WHEN (Act)
+        c = a + b; //System Under Test (SUT)
+
+        //THEN (Assert)
+        Assert.Equal(30, c);
+    }
+
+
+    [Theory]
+    [InlineData(10, 20, 30)]
+    [InlineData(4, 2, 6)]
+    public void CanAddAnyTwoIntegers(int a, int b, int expected)
+    {
+        int answer = a + b;
+
+        Assert.Equal(expected, answer);
+    }
+}
